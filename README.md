774696199cb7183c694f8977ed799110d8cc6188
Hector Escalante <hector.samuel.escalante@outlook.com>
2020-08-26 18:23:01 +0200

    add readme notes

--- README.md
+++ README.md
@@ -1,1 +1,49 @@
-# bigquery-serilog-sink
+# Serilog.Sinks.BigQuery
+
+[![Build status](https://dev.azure.com/hectorescalante/Github%20Projects/_apis/build/status/Serilog.Sinks.BigQuery)](https://dev.azure.com/hectorescalante/Github%20Projects/_build/latest?definitionId=6)
+
+## Usage
+
+```
+Log.Logger = new LoggerConfiguration()
+  ...
+  .WriteTo.BigQuery("my-gcp-project-id")
+  .CreateLogger();
+```
+
+Also with IConfiguration
+
+```
+Log.Logger = new LoggerConfiguration()
+  ...
+  .WriteTo.BigQuery(configuration.GetSection("BigQuery"))
+  .CreateLogger();
+```
+appsettings.json
+```
+{
+  "BigQuery": {
+    "ProjectId": "my-gcp-project-id",
+    "DatasetName": "mydataset"
+  }
+}
+```
+
+## Log structure
+
+The BigQuery table has the following structure.
+
+| Field name  | Type    |
+| ----------  | ----    |
+| Time 	      | STRING  |
+| LevelNumber |	INTEGER |
+| LevelName 	| STRING 	|
+| Message 	  | STRING 	|
+| Template 	  | STRING 	|
+| Properties 	| RECORD (REPEATED) |
+| Properties. Name  |	STRING |
+| Properties. Value | 	STRING |	
+| ExceptionStackTrace |	STRING |
+
+>The properties name column could have "." for nested properties in the form of "RootProperty.ChildProperty".
+
--- src/Serilog.Sinks.BigQuery/BigQuerySinkExtensions.cs
+++ src/Serilog.Sinks.BigQuery/BigQuerySinkExtensions.cs
@@ -8,21 +8,18 @@ namespace Serilog.Sinks.BigQuery
 {
   public static class BigQuerySinkExtensions
   {
-    public static LoggerConfiguration BigQuery(this LoggerSinkConfiguration sinkConfiguration, IConfigurationSection configuration)
+    public static LoggerConfiguration BigQuery(this LoggerSinkConfiguration sinkConfiguration, string projectId, string datasetName = "logs", LogEventLevel logLevel = LogEventLevel.Warning)
     {
-      return sinkConfiguration.BigQuery(configuration, LogEventLevel.Warning);
+      var sinkOptions = new BigQuerySinkOptions() { ProjectId = projectId, DatasetName = datasetName };
+      return sinkConfiguration.BigQuery(sinkOptions, logLevel);
     }
-    public static LoggerConfiguration BigQuery(this LoggerSinkConfiguration sinkConfiguration, IConfigurationSection configuration, LogEventLevel logLevel)
+    public static LoggerConfiguration BigQuery(this LoggerSinkConfiguration sinkConfiguration, IConfigurationSection configuration, LogEventLevel logLevel = LogEventLevel.Warning)
     {
       var sinkOptions = new BigQuerySinkOptions();
       configuration.Bind(sinkOptions);
       return sinkConfiguration.BigQuery(sinkOptions, logLevel);
     }
-    public static LoggerConfiguration BigQuery(this LoggerSinkConfiguration sinkConfiguration, BigQuerySinkOptions sinkOptions)
-    {
-      return sinkConfiguration.BigQuery(sinkOptions, LogEventLevel.Warning);
-    }
-    public static LoggerConfiguration BigQuery(this LoggerSinkConfiguration sinkConfiguration, BigQuerySinkOptions sinkOptions, LogEventLevel logLevel)
+    public static LoggerConfiguration BigQuery(this LoggerSinkConfiguration sinkConfiguration, BigQuerySinkOptions sinkOptions, LogEventLevel logLevel = LogEventLevel.Warning)
     {
       return sinkConfiguration.Sink(new BigQuerySink(sinkOptions, new BigQueryConnection(sinkOptions)), logLevel);
     }
